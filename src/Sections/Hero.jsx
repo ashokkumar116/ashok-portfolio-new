bfcad39d435487279f4f351a1ea0fbc647fc3bb4
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2025-12-30 21:46:03 +0530

    Basic Hero Outline

---
+++ src/Sections/Hero.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <section className='h-dvh'>
+        <div className='text-white flex flex-col justify-center items-center h-full text-center gap-4'>
+            <h1 className='text-7xl font-bold'>I help brands build a strong digital presence</h1>
+            <p className='text-xl'>End-to-end design and development, built with clarity, performance, and scale in mind</p>
+        </div>
+    </section>
+  )
+}
+
+export default Hero
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
+import gsap from 'gsap';
+import Navbar from './Components/Navbar.jsx';
+
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar />
     <App />
   </StrictMode>,
 )
