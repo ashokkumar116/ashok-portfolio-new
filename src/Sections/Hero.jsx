25fdc7052bd7ac8f3966b0596220c63eae132937
ASHOK KUMAR P <ashokkumarpandian7@gmail.com>
2026-01-02 23:30:44 +0530

    Add Hero to about Transition animation and Modify Hero Contents

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -6,6 +6,7 @@ import CursorPinkSplash from '../Components/CursorPinkSplash'
 import DotMatrixBg from '../Components/DotMatrixBg'
 import GrainOverlay from '../Components/GrainOverlay'
 import SoftGradientMesh from '../Components/SoftGradientMesh'
+import ashok from '../assets/ash1.jpg'
 
 const Hero = () => {
 
@@ -105,10 +106,6 @@ const Hero = () => {
             type: isMobile ? "words" : "chars",
         })
 
-        const splitsubtitle = SplitText.create(".subtitle", {
-            type: isMobile ? "words" : "chars",
-        })
-
         // Optimized animation for mobile
         tl.from(isMobile ? splittitle.words : splittitle.chars, {
             scale: isMobile ? 1.1 : 2.5,
@@ -119,60 +116,37 @@ const Hero = () => {
             textShadow: isMobile ? "none" : "0 0 25px rgba(255,0,150,0.8)",
             force3D: true, // Hardware acceleration
         })
-        .from(isMobile ? splitsubtitle.words : splitsubtitle.chars, {
-            scale: isMobile ? 1.1 : 2,
-            opacity: 0,
-            filter: isMobile ? "none" : "blur(10px)",
-            stagger: isMobile ? 0.02 : 0.01,
-            ease: "power2.out",
-            force3D: true,
-        }, "-=0.4")
-        .from(".btn-1", {
-            opacity: 0,
-            filter: isMobile ? "none" : "blur(10px)",
-            y: isMobile ? 10 : 0,
-            x: isMobile ? 0 : -20,
-            ease: "power2.out",
-            force3D: true,
-        }, "-=0.6")
-        .from(".btn-2", {
-            opacity: 0,
-            filter: isMobile ? "none" : "blur(10px)",
-            y: isMobile ? 10 : 0,
-            x: isMobile ? 0 : 20,
-            ease: "power2.out",
-            force3D: true,
-        }, "-=0.6")
 
         return () => {
             splittitle.revert()
-            splitsubtitle.revert()
         }
     }, [isMobile])
 
   return (
-    <section className='h-dvh flex flex-col justify-center items-center gap-10 overflow-hidden' id='hero'>
+    <section className='h-screen flex flex-col justify-center items-center gap-10 overflow-hidden' id='hero'>
         {!isMobile && <CursorPinkSplash />}
         <DotMatrixBg />
         <GrainOverlay />
         <SoftGradientMesh />
-        <div className='text-white flex flex-col justify-center items-center text-center gap-10 w-[80%] mx-auto '>
-            <h1 className='text-3xl md:text-7xl font-bold title'>I help brands build a strong <br /> digital presence</h1>
-            <p className='text-sm md:text-xl subtitle'>End-to-end design and development, built with clarity, performance, and scale in mind</p>
+        <div className='absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10' id='img-container'>
+            <img src={ashok} alt="" className='w-0 h-0 rounded-full object-cover' id='ashok-img' />
         </div>
-        <div className="flex gap-5 btns">
-            {/* Filled Button */}
+        <h1 id='intro-name' className='text-3xl md:text-7xl font-bold absolute left-130  text-white'>Hi, I’m Ashok Kumar</h1>
+        <div className='text-white flex flex-col justify-center items-center text-center gap-10 w-[80%] mx-auto '>
+            <h1 className='text-3xl md:text-7xl font-bold title'>Looking for a <span className="text-pink-500">website</span> that <br />actually helps your business<span className="text-pink-500"> grow?</span></h1>
+            <p className='text-sm md:text-5xl font-bold subtitle absolute-center'>You’re exactly where you need to be.</p>
+        </div>
+        {/* <div className="flex gap-5 btns">
             <button className="btn-1 group cursor-pointer relative overflow-hidden rounded-2xl bg-pink-500 px-6 py-3 font-medium text-white gsap-btn">
                 <span className="pointer-events-none relative z-10">Start a project</span>
                 <span className="absolute inset-0 bg-pink-600 gsap-bg rounded-xl" />
             </button>
 
-            {/* Outline Button */}
             <button className="btn-2 group cursor-pointer relative overflow-hidden rounded-2xl border-2 border-pink-500 px-6 py-3 font-medium text-pink-500 gsap-btn">
                 <span className="pointer-events-none relative z-10 gsap-text max-md:text-white max-md:text-center">View my work</span>
                 <span className="absolute inset-0 bg-pink-500 gsap-bg rounded-xl" />
             </button>
-        </div>
+        </div> */}
 
     </section>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import { ScrollSmoother, ScrollTrigger } from 'gsap/all';
+import { Flip, ScrollSmoother, ScrollTrigger } from 'gsap/all';
 import gsap from 'gsap';
 import Navbar from './Components/Navbar.jsx';
 
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+gsap.registerPlugin(ScrollTrigger, ScrollSmoother, Flip);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
